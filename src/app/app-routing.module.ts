92cf9261e5b5f133426d737d635b1187565fe877
lucasvasconcelo78 <lucas.vasconcelos@b2mlportal.com.br>
2021-10-01 16:59:26 -0300

    criando a roda do editor de texto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { EditTextComponent } from './components/edit-text/edit-text.component';
 
 const routes: Routes = [{
   path:"",
@@ -10,8 +11,11 @@ const routes: Routes = [{
 {
   path: "calculator",
   component: CalculatorComponent
-}
-];
+},
+{
+  path:"editText",
+  component: EditTextComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
